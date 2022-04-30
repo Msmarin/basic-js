82dd81d4d1293e990b985719da4c83fe9c659185
Msmarin <msmetelskaya@gmail.com>
2022-04-30 14:01:03 +0300

    decided: common-character-count, count-cats,dream-team

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getCommonCharacterCount( s1, s2) {
+    let arr = Array.from(s2);
+    let count=0;
+    let ar = Array.from(s1);
+    
+    for(let j=0;j<arr.length;j++){    
+    for(let i=0;i<ar.length;i++){
+        if (arr[j]==ar[i]){
+        ar.splice(i,1);
+        arr.splice(j,1);
+    count+=1;
+    i--;
+    j--;
+    console.log(arr);
+    console.log(ar);
+    }
+    }
+    
+    };
+    return count;  
+            }     
+
 
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
-module.exports = {
-  DepthCalculator
-};
+    // if (!Array.isArray(arr) || arr.length == 0) return 0;
+
+    calculateDepth(arr) {
+        let one = ['['];
+        let pair = { ['[']: ']' };
+        let stack = [];
+        let count = 0
+        for (let i = 0; i < arr.length; i++){
+            let cur = arr[i];
+        if (one.includes(cur)) {
+            stack.push(cur);
+        } else {
+            if (stack.length === 0) {
+                return stack.length;
+            }
+        }
+            let top = stack[stack.length - 1];
+            if (pair[cur] === top) {
+                count++;
+                stack.pop();
+                
+            }
+        
+            return stack.length;
+        }
+    
+    }
+}
+        // for (let item of arr) {
+        //     if (!Array.isArray(item) || item.length==0) return 0;
+        // };
+        // let calc = arr.map(i => i.length).sort((a, b) => a - b);
+        // for (let i = 0; i < calc.length - 1; i++){
+        // if (calc[i + 1] - calc[i] > 1 ) return (calc[i] + 1);
+        // }
+        module.exports = {
+            DepthCalculator
+        };
+    
